1004a2c48a74a62bd4cdb49dbe5d673ab8fc2e90
boikostas <boikostas3@gmail.com>
2024-07-24 22:34:21 +0300

    Project created

---
+++ Tic-Tac-Toe/Tic-Tac-Toe/Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeApp.swift
+//  Tic-Tac-Toe
+//
+//  Created by Stas Boiko on 24.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
