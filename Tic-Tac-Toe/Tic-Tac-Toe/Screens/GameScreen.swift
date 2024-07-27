c9cabd36516c0a7bafdee8a1398a0d9f36deb555
boikostas <boikostas3@gmail.com>
2024-07-27 14:54:51 +0300

    added win logic + draw logic, simple computer turn logic

--- Tic-Tac-Toe/Tic-Tac-Toe/Screens/GameScreen.swift
+++ Tic-Tac-Toe/Tic-Tac-Toe/Screens/GameScreen.swift
@@ -32,32 +32,36 @@ struct GameScreen: View {
        ]
     
     @State private var boardCells: [[SignType]] = [[.none, .none, .none],
-                                                 [.none, .none, .none],
-                                                 [.none, .none, .none]]
+                                                   [.none, .none, .none],
+                                                   [.none, .none, .none]]
     
     @State private var currentPlayerSing: SignType? = nil
-    var gameType: GameType = .pvp
+    var gameType: GameType = .pve
     
     @State private var turnLabel = "Your turn"
+    @State private var enableToTap: Bool = true
     
     var body: some View {
         ZStack {
             BackgroundGradient()
+            
             VStack {
-                
-                Text(turnLabel)
-                    .foregroundStyle(.white)
-                    .fontWeight(.heavy)
-                    .font(.system(size: 32))
-                    .padding(.bottom, 50)
+                titleLabel
                 gameBoard
             }
-            
         }
         .ignoresSafeArea()
         .onAppear {
             startGame()
         }
+    }
+    
+    private var titleLabel: some View {
+        Text(turnLabel)
+            .foregroundStyle(.white)
+            .fontWeight(.heavy)
+            .font(.system(size: 32))
+            .padding(.bottom, 50)
     }
     
     private var gameBoard: some View {
@@ -82,14 +86,9 @@ struct GameScreen: View {
                                 boardCells[i][j] = currentPlayerSing
                             }
                         }
-                        if let sign = currentPlayerSing {
-                            if isWin(with: sign) {
-                                turnLabel = (sign == .x) ? "Player 1 wins!" : "Player 2 wins!"
-                                return
-                            }
-                        }
                         nextTurn()
                     }
+                    .disabled(!enableToTap)
                 }
             }
         }
@@ -132,15 +131,90 @@ struct GameScreen: View {
     private func nextTurn() {
         switch gameType {
         case .pvp:
+            if checkIfGameOver() {
+                return
+            }
             currentPlayerSing == .x ? (currentPlayerSing = .o) : (currentPlayerSing = .x)
             currentPlayerSing == .x ? (turnLabel = "PLAYER 1's TURN") : (turnLabel = "PLAYER 2's TURN")
         case .pve:
+            if checkIfGameOver() {
+                return
+            }
             currentPlayerSing == .x ? (currentPlayerSing = .o) : (currentPlayerSing = .x)
             currentPlayerSing == .x ? (turnLabel = "YOUR TURN") : (turnLabel = "🤖's TURN")
+            enableToTap = (currentPlayerSing == .x) ? true : false
+            
+            if currentPlayerSing == .o {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    self.computerTurn()
+                }
+            }
         }
     }
     
+    private func checkIfGameOver() -> Bool {
+        if checkIfDraw() {
+            return true
+        }
+        
+        if let sign = currentPlayerSing {
+            if isWin(with: sign) {
+                switch gameType {
+                case .pvp:
+                    turnLabel = (sign == .x) ? "Player 1 wins!" : "Player 2 wins!"
+                case .pve:
+                    turnLabel = (sign == .x) ? "You win!" : "🤖 wins!"
+                }
+                enableToTap = false
+                return true
+            }
+        }
+        
+        enableToTap = true
+        return false
+    }
+    
+    private func checkIfDraw() -> Bool {
+        if boardCells.contains(where: { $0.contains(where: { $0 == .none }) }) {
+            return false
+        }
+        
+        turnLabel = "Draw!"
+        enableToTap = false
+        return true
+    }
+    
+    private func computerTurn() {
+        //TODO: Make it clever!
+        outerLoop: for i in 0..<boardCells.count {
+            for j in 0..<boardCells[i].count {
+                if boardCells[i][j] == .none {
+                    if let sign = currentPlayerSing {
+                        boardCells[i][j] = sign
+                        break outerLoop
+                    }
+                }
+            }
+        }
+        nextTurn()
+    }
+    
     private func isWin(with sign: SignType) -> Bool {
+        var leftDiagonalCount = 0, rightDiagonalCount = 0
+        for i in 0..<boardCells.count {
+            for j in 0..<boardCells[i].count {
+                if i == j && boardCells[i][j] == sign {
+                    leftDiagonalCount += 1
+                }
+                if i == boardCells.count - 1 - j && boardCells[i][j] == sign {
+                    rightDiagonalCount += 1
+                }
+            }
+        }
+        if leftDiagonalCount == 3 || rightDiagonalCount == 3 {
+            return true
+        }
+        
         for i in 0..<boardCells.count {
             var hCount = 0, vCount = 0
             for j in 0..<boardCells[i].count {
@@ -151,11 +225,11 @@ struct GameScreen: View {
                     vCount += 1
                 }
             }
-            
             if vCount == 3 || hCount == 3 {
                 return true
             }
         }
+        
         return false
     }
 }
